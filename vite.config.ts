3374042a6dbe4ef8d4f6daabb8b934da8b92bf37
Mashfiq Naushad <120036145+NuhashMaq@users.noreply.github.com>
2025-08-12 01:07:29 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // 👈 must match repo name exactly (case sensitive)
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
